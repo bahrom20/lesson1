1398f50355bb0f384742b09bb3f0758aff6f58cc
bahrom20 <153849473+bahrom20@users.noreply.github.com>
2023-12-14 21:53:13 +0500

    Add files via upload

---
+++ dars 3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"                            a                      "<<endl;
+	cout<<"                          aaaa                      "<<endl;
+	cout<<"                        aaaaaaa                      "<<endl;
+	cout<<"                      aaaaaaaaaa                      "<<endl;
+	cout<<"                    aaaaaaaaaaaaa                      "<<endl;
+	cout<<"                    aaaaaaaaaaa                      "<<endl;
+	cout<<"                     aaaaaaaaa                      "<<endl;
+	cout<<"                aaa      aaaa   aaa                   "<<endl;
+	cout<<"             aaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa          "<<endl;
+	cout<<"     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           "<<endl;
+	cout<<"          aaaaaa     aaaaaaa   aaaaaaa        "<<endl;
+	cout<<"           aaaa       aaaaa      aaaa          "<<endl;
+	
+	return 0;
+	
+	
+	
+}
---
+++ dars 5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"      xxxxxx           xx                  xx         xxxxxx        x    xxxxx    "<<endl;
+	cout<<"    xxx    xxx      xxxxx               xxxxx       xxx    xxx     xx        x   "<<endl;
+	cout<<"   xx       xx         xx       xxx        xx      xx       xx    x x       x    "<<endl;
+	cout<<"            xx         xx       xxx        xx               xx   xxxxx     x     "<<endl;
+	cout<<"          xxx          xx                  xx             xxx       x      x   "<<endl;
+	cout<<"        xxx            xx                  xx           xxx         x      x   "<<endl;
+	cout<<"      xxx              xx                  xx         xxx       "<<endl;
+	cout<<"    xxx                xx                  xx       xxx      "<<endl;
+	cout<<"   xx                  xx       xxx        xx      xx    "<<endl;
+	cout<<"  xx                   xx       xxx        xx      xx   "<<endl;
+	cout<<"  xx                   xx                  xx      xx              "<<endl;
+	cout<<"  xxxxxxxxxxxx      xxxxxxxx            xxxxxxxx   xxxxxxxxxxx     "<<endl;
+	
+	return 0;
+}
